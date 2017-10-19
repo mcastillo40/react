3d28198c7e1affecffa986518490116764479a62
mcastillo40 <chgreen08@gmail.com>
2017-10-19 12:05:33 -0700

    DOM practice before installing babel

--- react/indecision-app/public/scripts/app.js
+++ react/indecision-app/public/scripts/app.js
@@ -2,11 +2,13 @@ console.log("App working");
 
 // JSX - Javascript XML
 //var template = <p>This is JSX from app.js</p>;
-var template = React.createElement(
-    "p",
-    { id: "new" },
-    "This is JSX"
-  );
+
+//var template = React.createElement(
+//    "p",
+//    { id: "new" },
+//    "This is JSX"
+//); 
+
 var appRoot = document.getElementById("app");
 
 ReactDom.render(template, appRoot);
