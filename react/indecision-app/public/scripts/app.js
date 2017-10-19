d9c05c48a21c19d12d9759a96d951ab389b4289d
mcastillo40 <chgreen08@gmail.com>
2017-10-19 11:57:08 -0700

    test react dom

---
+++ react/indecision-app/public/scripts/app.js
@@ -0,0 +1,12 @@
+console.log("App working");
+
+// JSX - Javascript XML
+//var template = <p>This is JSX from app.js</p>;
+var template = React.createElement(
+    "p",
+    { id: "new" },
+    "This is JSX"
+  );
+var appRoot = document.getElementById("app");
+
+ReactDom.render(template, appRoot);
