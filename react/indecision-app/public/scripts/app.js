de85e42503a8c380fc7cac23ed1126a362800409
mcastillo40 <chgreen08@gmail.com>
2017-10-19 22:38:48 -0700

    Install babel

--- react/indecision-app/public/scripts/app.js
+++ react/indecision-app/public/scripts/app.js
@@ -1,14 +1,14 @@
+"use strict";
+
 console.log("App working");
 
 // JSX - Javascript XML
-//var template = <p>This is JSX from app.js</p>;
+var template = React.createElement(
+  "h1",
+  null,
+  "This changed app.js"
+);
 
-//var template = React.createElement(
-//    "p",
-//    { id: "new" },
-//    "This is JSX"
-//); 
+var appRoot = document.getElementById('app');
 
-var appRoot = document.getElementById("app");
-
-ReactDom.render(template, appRoot);
+ReactDOM.render(template, appRoot);
---
+++ react/indecision-app/src/app.js
@@ -0,0 +1,8 @@
+console.log("App working");
+
+// JSX - Javascript XML
+var template = <h1>This changed app.js</h1>;
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
