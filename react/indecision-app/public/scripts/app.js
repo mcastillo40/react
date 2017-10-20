6f8507af1990ed5176b49263ed68f1ad2fa42d20
mcastillo40 <chgreen08@gmail.com>
2017-10-20 01:12:15 -0700

    more testing

--- react/indecision-app/public/scripts/app.js
+++ react/indecision-app/public/scripts/app.js
@@ -6,7 +6,7 @@ console.log("App working");
 var template = React.createElement(
   "h1",
   null,
-  "This also changed seven app.js"
+  "This also changed the app.js"
 );
 
 var appRoot = document.getElementById('app');
