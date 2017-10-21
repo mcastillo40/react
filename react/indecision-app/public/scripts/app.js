d075575318b77f6d02297fcb82ee92d9eb234903
mcastillo40 <chgreen08@gmail.com>
2017-10-20 20:52:43 -0700

    Using javascript expressions

--- react/indecision-app/public/scripts/app.js
+++ react/indecision-app/public/scripts/app.js
@@ -1,43 +1,75 @@
-"use strict";
+'use strict';
 
 console.log("App working");
 
+// Create app object title/subtitle
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Some Info'
+};
+
 // JSX - Javascript XML
 var template = React.createElement(
-    "div",
+    'div',
     null,
     React.createElement(
-        "h1",
+        'h1',
         null,
-        "Matt C"
+        app.title
     ),
     React.createElement(
-        "p",
+        'p',
         null,
-        "some info"
+        app.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item 1'
+        )
     )
 );
 
+var user = {
+    name: 'Albert',
+    age: '30',
+    location: 'Oregon'
+};
+
+var userName = 'Matt';
+var age = 20;
+var userLocation = 'Orange County';
+
 var template2 = React.createElement(
-    "div",
+    'div',
     null,
     React.createElement(
-        "h1",
+        'h1',
         null,
-        "Matthew C"
+        user.name
     ),
     React.createElement(
-        "p",
+        'p',
         null,
-        "Age: 20"
+        'Age: ',
+        age
     ),
     React.createElement(
-        "p",
+        'p',
         null,
-        "Location: Orange County"
+        'Location: ',
+        userLocation
     )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template2, appRoot);
+ReactDOM.render(template, appRoot);
--- react/indecision-app/src/app.js
+++ react/indecision-app/src/app.js
@@ -1,21 +1,41 @@
 console.log("App working");
 
+// Create app object title/subtitle
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Some Info'
+};
+
 // JSX - Javascript XML
 var template = (
     <div>
-        <h1>Matt C</h1> 
-        <p>some info</p>
+        <h1>{app.title}</h1> 
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>Item 1</li>
+            <li>Item 1</li>
+        </ol>
     </div>
 );
 
+var user = {
+    name: 'Albert',
+    age: '30',
+    location: 'Oregon'
+};
+
+var userName = 'Matt';
+var age = 20;
+var userLocation = 'Orange County';
+
 var template2 = (
     <div>
-        <h1>Matthew C</h1>
-        <p>Age: 20</p>
-        <p>Location: Orange County</p>
+        <h1>{user.name}</h1>
+        <p>Age: {age}</p>
+        <p>Location: {userLocation}</p>
     </div>
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template2, appRoot);
+ReactDOM.render(template, appRoot);
