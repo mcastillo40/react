65fe1cafc777df051dd97e92a6a99c909946fe32
matman40 <chgreen08@gmail.com>
2017-10-30 22:05:56 -0700

    Create form to add items to an array as well as creating a button to remove all items

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -6,46 +6,89 @@ console.log("App working");
 var app = {
     title: 'Indecision App',
     subtitle: 'Life is Amazing',
-    options: ['One', 'Two']
+    options: []
 };
 
-// JSX - Javascript XML 
-// The webpage template example
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item 2'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item 1'
-        )
-    )
-);
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+
+    var option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = "";
+        renderPage();
+    }
+};
+
+var removeAll = function removeAll() {
+
+    app.options = [];
+
+    renderPage();
+};
 
 // Section where template will be rendered
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+// JSX - Javascript XML 
+// The webpage template example
+var renderPage = function renderPage() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'button',
+            { onClick: removeAll },
+            'Remove all'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'Item 2'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'Item 1'
+            )
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
+        )
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+renderPage();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -4,30 +4,63 @@ console.log("App working");
 const app = {
     title: 'Indecision App',
     subtitle: 'Life is Amazing',
-    options: ['One', 'Two']
+    options: []
 };
 
-// JSX - Javascript XML 
-// The webpage template example
-const template = (
-    <div>
-        <h1>{app.title}</h1> 
+// Item submitted by user will be added to the options array
+// Will render the page again after being submitted
+// Validate that an item was entered and submitted prior to rendering the page
+const onFormSubmit = (e) => {
+    e.preventDefault();
 
-        {/*Validate that subtitle exists then display if available*/}
-        {(app.subtitle) && <p>{app.subtitle}</p>}
+    const option = e.target.elements.option.value;
 
-        {/*If options are available then inform user that options are available if not then inform user*/}
-        <p>{(app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
-        
-        <ol>
-            <li>Item 2</li>
-            <li>Item 1</li>
-        </ol>
-    </div>
+    if(option) {
+        app.options.push(option);
+        e.target.elements.option.value = "";
+        renderPage();
+    }
 
-);
+};
+
+// Removes all options placed in the array by user
+const removeAll = () => {
+    app.options = [];
+ 
+    renderPage();
+}
 
 // Section where template will be rendered
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+// JSX - Javascript XML 
+// The webpage template example
+const renderPage = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1> 
+    
+            {/*Validate that subtitle exists then display if available*/}
+            {(app.subtitle) && <p>{app.subtitle}</p>}
+    
+            {/*If options are available then inform user that options are available if not then inform user*/}
+            <p>{(app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+
+            <button onClick={removeAll}>Remove all</button> 
+
+            <ol>
+                <li>Item 2</li>
+                <li>Item 1</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button> 
+            </form>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+renderPage();
